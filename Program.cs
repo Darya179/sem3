486c0339ecdb75b44be3a21d612d5875ecad24e6
Darya <daryalas17@gmail.com>
2022-08-06 07:17:31 +0300

    seminar3_3

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+int Res(int nLoc)
+{
+
+    return nLoc*nLoc*nLoc;
+
+}
+
+for (int i = 1; i <= n; i++)
+{
+
+    Console.WriteLine(Res(i));
+}
